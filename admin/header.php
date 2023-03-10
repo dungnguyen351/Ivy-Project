7acac9ccf95f878589c7ea4cc35d560a837f50c1
dungnguyen351 <duynguyen030501@gmail.com>
2023-03-11 00:30:33 +0700

    Commit 11/3

--- admin/header.php
+++ admin/header.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Danh mục sản phẩm</title>
 </head>
 <body>
-    
-</body>
-</html>
+    <header>
+        <h1>TOP</h1>
+    </header>
---
+++ admin/slider.php
@@ -0,0 +1,23 @@
+<section class="admin-content">
+        <div class="admin-content-left">
+            <ul>
+                <li><a href="#">Danh mục</a>
+                    <ul>
+                        <li><a href="cartegoryadd.php">Thêm Danh mục</a></li>
+                        <li><a href="cartegorylist.php">Danh sách Danh mục</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Loại sản phẩm</a>
+                    <ul>
+                        <li><a href="">Thêm Loại sản phẩm</a></li>
+                        <li><a href="">Danh  Loại sản phẩm</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Sản phẩm</a>
+                    <ul>
+                        <li><a href="">Thêm sản phẩm</a></li>
+                        <li><a href="">Danh sách sản phẩm</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
