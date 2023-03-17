7a9f9184cd7a9d24d082e74bee62e019e2502cb1
dungnguyen351 <duynguyen030501@gmail.com>
2023-03-17 15:27:18 +0700

    commit 17/3

--- admin/class/product_class.php
+++ admin/class/product_class.php
@@ -23,55 +23,91 @@ class product {
         $result = $this ->db->select($query);
         return $result;
     }
+    public function insert_product() {
+        $product_name = $_POST['product_name'];
+        $cartegory_id = $_POST['cartegory_id'];
+        $brand_id = $_POST['brand_id'];
+        $product_price = $_POST['product_price'];
+        $product_price_new = $_POST['product_price_new'];
+        $product_desc = $_POST['product_desc'];
+        $product_img = $_FILES['product_img']['name'];
+        $filetarget = basename($_FILES['product_img']['name']);
+        $filetype = strtolower(pathinfo($product_img,PATHINFO_EXTENSION));
+        $filesize = $_FILES['product_img']['size'];
+        if(file_exists("uploads/$filetarget")) {
+            $alert = "File đã tồn tại";
+            return $alert;
+        }
+        else {
+            if($filetype != "jpg" && $filetype != "png" && $filetype != "jpeg") {
+                $alert = "Chỉ chọn file jpg,png,jpeg";
+                return $alert;
+            }
+
+            else{
+                if ($filesize > 1000000) {
+                    $alert = "File không được lớn hơn 1MB";
+                    return $alert;
+                }
+                else {
+                    move_uploaded_file($_FILES['product_img']['tmp_name'],"uploads/".$_FILES['product_img']['name']);
+                        $query = "INSERT INTO tbl_product (
+                            product_name,
+                            cartegory_id,
+                            brand_id,
+                            product_price,
+                            product_price_new,
+                            product_desc,
+                            product_img)
+                            VALUES (
+                            '$product_name',
+                            '$cartegory_id',
+                            '$brand_id',
+                            '$product_price',
+                            '$product_price_new',
+                            '$product_desc',     
+                            '$product_img')";
+                        $result = $this ->db->insert($query);
+                        if($result){
+                        $query = "SELECT * FROM tbl_product ORDER BY product_id DESC LIMIT 1";
+                        $result = $this -> db -> select($query)->fetch_assoc();
+                        $product_id = $result['product_id'];
+                        $filename = $_FILES['product_img_desc']['name'];
+                        $filetmp = $_FILES['product_img_desc']['tmp_name'];
+            
+                        foreach($filename as $key => $value)
+                        {
+                            move_uploaded_file($filetmp[$key],"uploads/". $value);
+                            $query = "INSERT INTO tbl_product_img_desc (product_id,product_img_desc) VALUE ('$product_id','$value')";
+                            $result = $this ->db->insert($query);
+                        }
+                    }
+                }
+            }
+                
+            
+        }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public function insert_brand($cartegory_id,$brand_name){
-        $query = "INSERT INTO tbl_brand (cartegory_id,brand_name) VALUES ('$cartegory_id','$brand_name')";
-        $result = $this ->db->insert($query);
-        header('location:brandlist.php');
+        
+        //header('location:brandlist.php');
         return $result;
     }
     
    
+
+
+
+
+
+
+
+
+
+
+
+
+
     public function get_brand($brand_id) {
         $query = "SELECT * FROM tbl_brand WHERE brand_id = '$brand_id'";
         $result = $this ->db->select($query);
--- admin/productadd.php
+++ admin/productadd.php
@@ -7,10 +7,10 @@ include "class/product_class.php"
 $product = new product;
 if($_SERVER['REQUEST_METHOD']=== 'POST'){
     //var_dump($_POST,$_FILES);
-    echo '<pre>';
-    echo print_r($_POST);
-    echo '</pre>';
-    //$insert_brand = $brand ->insert_brand($cartegory_id,$brand_name); 
+    // echo '<pre>';
+    // echo print_r($_FILES['product_img_desc']['name']);
+    // echo '</pre>';
+    $insert_product = $product ->insert_product($_POST,$_FILES); 
 }
 ?>
 
@@ -21,7 +21,7 @@ if($_SERVER['REQUEST_METHOD']=== 'POST'){
                     <label for="" placeholder="">Nhập tên sản phẩm<span style="color: red;" >*</span></label>
                     <input name="product_name" required type="text">
                     <label for="" placeholder="">Chọn danh mục<span style="color: red;" >*</span></label>
-                    <select name="cartegory_id" id="">
+                    <select name="cartegory_id" id="cartegory_id">
                     <option value="#">--Chọn--</option>
                         <?php 
                         $show_cartegory = $product -> show_cartegory();
@@ -33,27 +33,23 @@ if($_SERVER['REQUEST_METHOD']=== 'POST'){
                         ?>
                     </select>
                     <label for="" placeholder="">Chọn Loại sản phẩm<span style="color: red;" >*</span></label>
-                    <select name="brand_id" id="">
+                    <select name="brand_id" id="brand_id">
                         <option value="#">--Chọn--</option>
-                        <?php 
-                        $show_brand = $product -> show_brand();
-                        if($show_brand) {while($result = $show_brand ->fetch_assoc()){
-                        ?>
-                        <option value="<?php echo $result['brand_id'] ?>"><?php echo $result['brand_name'] ?></option>
-                        <?php 
-                        }}
-                        ?>
+                        
                     </select>
                     <label for="" placeholder="">Giá sản phẩm<span style="color: red;" >*</span></label>
                     <input name="product_price"  required type="text">
                     <label for="" placeholder="">Giá khuyến mãi<span style="color: red;" >*</span></label>
-                    <input name="Product_price_new" required type="text">
+                    <input name="product_price_new" required type="text">
                     <label for="" placeholder="">Mô tả sản phẩm<span style="color: red;" >*</span></label> 
-                    <textarea name="product_desc" required id="" cols="30" row="10" placeholder="Mô tả sản phẩm"></textarea>
+                    <textarea name="product_desc" required id="editor1" cols="30" row="10" placeholder="Mô tả sản phẩm"></textarea>
                     <label for="" placeholder="">Ảnh sản phẩm<span style="color: red;" >*</span></label> 
+                    <span style="color:red"><?php if (isset($insert_product)) {
+                        echo ($insert_product);
+                    } ?></span>
                     <input name="product_img" required type="file">
                     <label for="" placeholder="">Ảnh mô tả<span style="color: red;" >*</span></label> 
-                    <input name="product_img_desc" required multiple type="file">
+                    <input name="product_img_desc[]" required multiple type="file">
                     <button type="submit">Thêm</button>
                 </form>
             </div>
@@ -61,4 +57,14 @@ if($_SERVER['REQUEST_METHOD']=== 'POST'){
     </section>
 
 </body>
+
+
+        <script>
+            CKEDITOR.replace('editor1', {
+                filebrowserBrowserUrl: 'ckfinder/ckfinder.html',
+                filebrowserUploadUrl:  'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files'
+            });
+
+        </script>
+
 </html>
